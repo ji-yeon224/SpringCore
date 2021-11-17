1a6036cfd1197f9d1882414bd62c94dbe42a3a6b
Kim-jiyeon224 <shjh0107@gmail.com>
2021-11-18 00:38:33 +0900

    AppConfig를 이용한 관심사 분리

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,12 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository; //인터페이스만! appconfig에서 구현체 지정
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     @Override
     public void join(Member member) {
         memberRepository.save(member);
