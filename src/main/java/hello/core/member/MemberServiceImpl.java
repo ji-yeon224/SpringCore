ba58f5fcfef0c73e20fb982ddf397aadcb86f574
ji-yeon224 <ji030yeon224>
2021-11-26 15:51:15 +0900

    싱글톤 컨테이너

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,14 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository; //인터페이스만! appconfig에서 구현체 지정
 
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
